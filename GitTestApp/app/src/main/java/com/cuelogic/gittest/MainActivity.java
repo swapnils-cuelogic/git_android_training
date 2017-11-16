cf55b9c099c339ec3a70a365b263e4183c77aaa7
anandl-cuelogic <anandl@cuelogic.com>
2017-11-16 08:53:45 +0530

    adding comment on feture1

--- GitTestApp/app/src/main/java/com/cuelogic/gittest/MainActivity.java
+++ GitTestApp/app/src/main/java/com/cuelogic/gittest/MainActivity.java
@@ -10,5 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toaster.showShort(this, "Welcome Message");
+        //adding comments to mainActivity
     }
 }
