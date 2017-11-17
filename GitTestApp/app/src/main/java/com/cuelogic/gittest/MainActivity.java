bca039392aae723b663ab65c117e4c8b1a6e186b
anandl-cuelogic <anandl@cuelogic.com>
2017-11-17 08:36:10 +0530

    -m adding message

--- GitTestApp/app/src/main/java/com/cuelogic/gittest/MainActivity.java
+++ GitTestApp/app/src/main/java/com/cuelogic/gittest/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+// adding text
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
