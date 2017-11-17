7793a60ac883df0fefb261f01ef555494c041d8a
Swapnil S <swapnil.sonar@cuelogic.co.in>
2017-11-17 11:00:15 +0530

    added fiest comment

--- GitTestApp/app/src/main/java/com/cuelogic/gittest/MainActivity.java
+++ GitTestApp/app/src/main/java/com/cuelogic/gittest/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toaster.showShort(this, "Addition: " + addition(4, 5));
+        //added first message
     }
 
     private int addition(int num1, int num2) {
